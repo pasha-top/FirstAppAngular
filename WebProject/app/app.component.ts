0082f13ac3bf6900160f2720835b04b2c5bc4ef7
Yaroshevich <pasha1rab@gmail.com>
2016-12-11 20:39:05 +0300

    second step selected hero

--- WebProject/app/app.component.ts
+++ WebProject/app/app.component.ts
@@ -11,9 +11,23 @@ import {Hero} from "./models/hero";
 
 export class AppComponent {
     title = 'My First Angular 2 App "Heroes"';
-    hero: Hero={
-        id: 1,
-        name: "Pasha"
-    };
+    selectedHero: Hero;
+    heroes = HEROES;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 
