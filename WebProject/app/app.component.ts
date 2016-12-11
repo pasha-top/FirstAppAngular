835f38148a99f87e772e15840e2cc62387da98b2
Yaroshevich <pasha1rab@gmail.com>
2016-12-11 22:58:18 +0300

    four step added hero service

--- WebProject/app/app.component.ts
+++ WebProject/app/app.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import {Hero} from "./models/hero";
+import { HeroService } from './services/hero.service';
 
 @Component({
     selector: 'my-app',
@@ -12,22 +13,22 @@ import {Hero} from "./models/hero";
 export class AppComponent {
     title = 'My First Angular 2 App "Heroes"';
     selectedHero: Hero;
-    heroes = HEROES;
+    heroes = Hero[];
+
+    constructor(private heroService: HeroService) { }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    };
+
+    ngOnInit(): void {
+        this.getHeroes();
+    };
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
-    }
+    };
+
 }
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
