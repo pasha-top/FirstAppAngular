7f1023cb190bbc9ea1cd7353cb07a7a13aab8191
Yaroshevich <pasha1rab@gmail.com>
2016-12-13 09:58:06 +0300

    added routing

--- WebProject/app/app.module.ts
+++ WebProject/app/app.module.ts
@@ -1,22 +1,43 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }   from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import {HeroService} from "./services/hero.service";
 import {HeroComponent} from "./hero/hero.component"
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([{
+            path: 'heroes',
+            component: HeroComponent
+        }, {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+        {
+            path: '',
+            redirectTo: '/dashboard',
+            pathMatch: 'full'
+        }
+
+        ])
+    ],
     declarations: [
         AppComponent,
         HeroComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        DashboardComponent
     ],
     providers: [
         HeroService
     ],
     bootstrap: [AppComponent]
 })
+    
 export class AppModule { }
---
+++ WebProject/app/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: '/app/dashboard/dashboard.component.template.html'
+
+})
+export class DashboardComponent { }
